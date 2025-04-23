eda6a52db48385a45d12d7d6e270ccafb68e5b50
UnlimitedLyfes <130376264+UnlimitedLyfes@users.noreply.github.com.>
2025-04-23 23:14:47 +0800

    finished next button i think, minor issues remain

--- scripts/content.js
+++ scripts/content.js
@@ -1,25 +1,12 @@
 console.log("autonext content.js working")
 
-const waitForNext = (selector, callback) => {           
-    const observer = new MutationObserver(() => {  // create observer and do this if it finds something changed
-        const target = document.querySelector(selector)
-        if(target){
-            observer.disconnect()
-            setTimeout(() => callback(target), 0) //wait for observer to disconnect kasi nag loloop siya parang tanga, 0 waittime since hindi priority ang setTimeout (macrotask)
-        }
-    })
-    
-    observer.observe(document.body, {       // observe document.body
-        childList: true, //watch for children being added or removed
-        subtree: true // watch not just immediate children, but all nested elements too
-    })
-}
-
 
 
 waitForNext(".module-sequence-footer-right", (target) => {
     const nextDiv = target
     console.log("Found nextDiv")
+
+    checkIfNextShouldStop()
 
     const nextButton = nextDiv.querySelector(".css-iclrqq-view--inlineBlock-baseButton")
 
@@ -33,11 +20,33 @@ waitForNext(".module-sequence-footer-right", (target) => {
     
 
 
-    nextDiv.appendChild(createSkipElement())
+    nextDiv.appendChild(createSkipElement(nextButton))
 
 })
 
-function createSkipElement(){
+
+function waitForNext(selector, callback) {           
+    const observer = new MutationObserver(() => {  // create observer and do this if it finds something changed
+        const target = document.querySelector(selector)
+        if(target){
+            observer.disconnect()
+            clearTimeout(timeoutForMissingNext)
+            setTimeout(() => callback(target), 0) //wait for observer to disconnect kasi nag loloop siya parang tanga, 0 waittime since hindi priority ang setTimeout (macrotask)
+        }
+    })
+    
+    observer.observe(document.body, {       // observe document.body
+        childList: true, //watch for children being added or removed
+        subtree: true // watch not just immediate children, but all nested elements too
+    })
+
+    const timeoutForMissingNext = setTimeout(() =>{
+        chrome.storage.local.set({ skipState: "false" })
+    }, 2000)
+}
+
+
+function createSkipElement(nextButton){
     const newElement = document.createElement("input")
     newElement.type = "image";
     newElement.src = `${chrome.runtime.getURL('icon.png')}`
@@ -46,7 +55,15 @@ function createSkipElement(){
         newElement.addEventListener("click", () => {
         chrome.storage.local.set({ skipState: "true" })
         console.log(`clicked`)
+        nextButton.click()
     })
     return newElement
 }
 
+function checkIfNextShouldStop(){
+    const checkThisForFinishNext = document.querySelector('.lock_explanation')
+    if(checkThisForFinishNext){
+        chrome.storage.local.set({ skipState: "false" })
+        console.log(`stopping nexts`)
+    }
+}
